e85266ae241d2609f0c3ce6b02a28666e1200b5d
Adam Magaluk <AdamMagaluK@gmail.com>
2015-01-28 19:25:11 -0500

    Using resin id as unique id

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var zetta = require('zetta');
 
-console.log(process.env)
-
 zetta()
+  .name(process.env.RESIN_DEVICE_UUID)
   .link(process.env.ZETTA_CLOUD.split(','))
   .listen(process.env.PORT || 0)
