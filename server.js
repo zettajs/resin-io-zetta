a0aa3fde249c9682a2cd08be139d49cb7efd4589
Adam Magaluk <AdamMagaluK@gmail.com>
2015-01-28 19:45:30 -0500

    Added hue driver

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var zetta = require('zetta');
+var Hue = require('zetta-hue-driver');
 
 zetta()
   .name(process.env.RESIN_DEVICE_UUID)
   .link(process.env.ZETTA_CLOUD.split(','))
+  .use(Hue)
   .listen(process.env.PORT || 0)
